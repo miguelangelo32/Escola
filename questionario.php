bd3254a10caf78e0e6aa6948c70288dfd5151d89
miguelangelo32 <80643977+miguelangelo32@users.noreply.github.com>
2021-03-19 17:56:52 +0000

    Add files via upload

---
+++ questionario.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>questionario</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><img src="LogoAEAAAcor.jpg" width="378" height="158" alt=""/></p>
+<p>&nbsp;</p>
+<h1 style="text-align: center; font-size: 36px; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, sans-serif; font-style: inherit; font-weight: bold;"><strong>QUESTIONÁRIO</strong>S</h1>
+<p style="text-align: center">&nbsp;</p>
+<p style="text-align: center"><a href="https://forms.gle/A3pacSxaJsZh19LMA" target="new"><img src="bt1.png" width="329" height="118" alt=""/></a></p>
+<p style="text-align: center">&nbsp;</p>
+<p style="text-align: center"><a href="https://forms.gle/QzPmC9T8nWCrnGZXA" target="new"><img src="bt2.jpg" width="332" height="123" alt=""/></a></p>
+<p style="text-align: center">&nbsp;</p>
+<p style="text-align: center">&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p style="text-align: center; font-size: 24px;">&nbsp;</p>
+</body>
+</html>
